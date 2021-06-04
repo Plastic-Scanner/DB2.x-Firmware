14234c3773d9e2de7fa4ad14f425ab0a1d8442eb
Jure Vidmar <vidmarjur@gmail.com>
2021-06-04 23:02:28 +0200

    ads1256: unit test read_id

--- test/test_ads1256.cpp
+++ test/test_ads1256.cpp
@@ -5,14 +5,18 @@
 
 void test_read_id()
 {
-
+    SPI.begin();
+    ADS1256 ads1256;
+    int id = ads1256.read_id();
+    TEST_ASSERT_EQUAL_INT(15, id);
 }
 
 
 void setup()
 {
     UNITY_BEGIN();
-    SPI.begin();
-    ADS1256 ads1256;
+    RUN_TEST(test_read_id);
     UNITY_END();
 }
+
+void loop() {}
