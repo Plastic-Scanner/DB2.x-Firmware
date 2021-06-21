226604f56d3140adfc1ebdb077b37ae9cd5029c3
Jure Vidmar <vidmarjur@gmail.com>
2021-06-21 16:47:19 +0200

    led: testing with three LEDS. One is toggling out of sync, how come?

--- src/main.cpp
+++ src/main.cpp
@@ -10,34 +10,30 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-const int NUM_LEDS = 4;
-
 ADS1256 adc;
-// Led leds[] = {
-//     Led(1),
-//     Led(2),
-//     Led(3)
-// };
-Led a(2);
-Led b(3);
-Led c(7);
+Led leds[] = {
+    Led(2),
+    Led(3),
+    Led(7)
+};
 
 void setup()
 {
     Serial.begin(9600);
     SPI.begin();
     adc.begin();
-    a.begin();
-    b.begin();
-    c.begin();
+
+    // Initialize LEDs
+    for (Led& l : leds) {
+        l.begin();
+    }
     Serial.println("PlasticScanner is initialized!");
 }
 
 void loop()
 {
-    a.toggle();
-    b.toggle();
-    c.toggle();
+    for (Led& l : leds) {
+        l.toggle();
+    }
     delay(1000);
-    Serial.println("loop");
 }
