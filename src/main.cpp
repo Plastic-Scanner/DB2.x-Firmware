86b6a1a8fad194dd7168800e6330a1cc655a20c3
Jure Vidmar <vidmarjur@gmail.com>
2022-02-14 17:56:14 +0100

    Revert "Minimal ads1256 lib example, works when using adc.begin(drate,gain,buffen)"
    This reverts commit 49baec26f3c0427bb125b1238016dcd9208f7cd9.

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,44 @@
+/*
+    Arduino firmware for the breakoutboard
+    Run with: 
+        pio run --target upload && pio device monitor
+*/
+
+#include "assert.h"
 #include <ADS1256.h>
+#include "led.h"
+#include <Arduino.h>
 #include <SPI.h>
 
-float clockMHZ = 8.0;
-float vRef = 2.5;
+static const int CLKSPEED_MHZ = 8;
+static const int VREF = 2.5;
 
-ADS1256 adc(clockMHZ,vRef,false);
-
-float sensor1;
-
+ADS1256 adc(CLKSPEED_MHZ, VREF, false);
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("Starting ADC");
+    Serial.begin(9600);
+    Serial.println("Initializing...");
+    SPI.begin();
+    adc.begin();
+    adc.setChannel(0);
+    Serial.println("PlasticScanner is initialized!");
 
-//   adc.begin(ADS1256_DRATE_30000SPS,ADS1256_GAIN_1,false); 
-  adc.begin();
-  Serial.println("ADC Started");
-
-  adc.setChannel(0,1);
+    // int id = 0;
+    // id = adc.read_id();
+    // Serial.print("ID: ");
+    // Serial.println(id);
 }
 
 void loop()
-{ 
-  adc.waitDRDY();
-  sensor1 = adc.readCurrentChannel();
-  Serial.println(sensor1, 10);
+{
+    float val = 0;
+
+    adc.waitDRDY();
+    val = adc.readCurrentChannel();
+  
+    Serial.print("channel: ");
+    Serial.println(val);
+
+    delay(500);
 }
