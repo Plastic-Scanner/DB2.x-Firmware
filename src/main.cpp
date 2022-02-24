eba1948430828160b7bc4184e26947238dfdb7e7
Jure Vidmar <vidmarjur@gmail.com>
2022-02-24 17:08:21 +0100

    sketching packet handler, using external fifo lib, WIP

--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,38 @@
         pio run --target upload && pio device monitor
 */
 #include "assert.h"
-#include "ADS1256.h"
-#include "tlc59208.h"
-#include "led.h"
 #include <Arduino.h>
-#include <SPI.h>
-#include <Wire.h>
-
-static const int CLKSPEED_MHZ = 8;
-static const float VREF = 2.5;
-
-ADS1256 adc(CLKSPEED_MHZ, VREF, false);
-TLC59208 leds;
 
 void setup()
 {
     Serial.begin(9600);
-    Serial.println("Initializing...");
-    SPI.begin();
-    Wire.begin();
-
-    // adc.begin(ADS1256_DRATE_30000SPS,ADS1256_GAIN_1,false); 
-    // adc.setChannel(0,1);
-    
-    leds.begin();
-    Serial.println("PlasticScanner is initialized!");
-
-    for (int i=0; i<8; i++) {
-        leds.on(i);
-        delay(500);
-        leds.off(i);
-    }
+    Serial.println("Initialized");
 }
+
+
+#include <CircularBuffer.h>
+
+const int header_chars = 2;
+CircularBuffer<char, 2> buff;
 
 void loop()
 {
+    // Sliding window on incoming serial data
+    int n = Serial.available();
+    if (n == 0) return;
+ 
+    if (buff.size() == header_chars) {
+        bool ret = buff.push(Serial.read());    // Shift left, read a single character
+        assert(ret == false);    
+        // According to the circularbuffer implementation, if it's overwriting returns true. Should always overwrite here.
+    } else {
+        // Fill the header_buffer (1st time)
+        while ((buff.size() < header_chars) && (n > 0)) {
+            buff.push((char)Serial.read());
+            n--;
+        }
+    }
 
+    // Compare
+    if ((buff.first() == 'D') && (buff.last() == 'B')) {Serial.println("GOT IT!");}
 }
