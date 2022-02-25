bcfd94e31b71e16bbfe6ac2e11a425b4cb3b12c9
Jure Vidmar <vidmarjur@gmail.com>
2022-02-25 14:50:24 +0100

    implement scan function

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,28 @@
 
 static const int CLKSPEED_MHZ = 8;
 static const float VREF = 2.5;
-ADS1256 adc(CLKSPEED_MHZ, VREF, false);
 static const int N_LEDS = 8;        // TODO: reorganize this duplicated constant @tlc
+
+ADS1256 adc(CLKSPEED_MHZ, VREF, false);
 TLC59208 ledctrl;
 Command commands;
 
 
 void scan()
 {
-    Serial.println("SCANNING!");
+    float readings[8] = {0};
+    for (int i=0; i<8; i++) {
+        ledctrl.on(i);
+        adc.waitDRDY(); 
+        readings[i] = adc.readCurrentChannel();
+        ledctrl.off(i);
+    }
+
+    for (int i=0; i<8; i++) {
+        Serial.print(readings[i], 4);
+        Serial.print('\t');
+    }
+    Serial.println();
 }
 
 void read_adc()
@@ -39,7 +52,7 @@ void ledon(int channel)
 
 void ledoff(int channel)
 {
-    if (channel >= 0 && channel <= N_LEDS-1) ledctrl.off(channel);
+    if (channel >= 0 && channel < N_LEDS) ledctrl.off(channel);
     else Serial.println("LEDOFF channel out of range");
 }
 
@@ -64,7 +77,7 @@ void setup()
         &unknown_command
     );
 
-    adc.setChannel(0,1);
+    adc.setChannel(0,1);    // differential ADC reading 
     Serial.println("PlasticScanner is initialized!");
 }
 
