319086c38affdecce6b655c97c091c06e31e014b
Jerry de Vos <devos.winsum@gmail.com>
2023-03-27 10:18:48 +0200

    take out adc

--- app/main.cpp
+++ app/main.cpp
@@ -5,7 +5,6 @@
 */
 #include "assert.h"
 #include "cli.h"
-#include "ADS1256.h"
 
 ///////////////////LED DRIVER////////////////////////////
 // If you are using the original Texas instruments led driver please use the following include
@@ -17,42 +16,25 @@
 PCA9551 ledDriver = PCA9551(PCA9551_ADDR_1);
 /////////////////////////
 #include <Arduino.h>
-#include <SPI.h>
 #include <Wire.h>
 
-static const int CLKSPEED_MHZ = 8;
-static const float VREF = 2.5;
-
-ADS1256 adc(CLKSPEED_MHZ, VREF, false);
 Cli cli;
 
 void scan(int argc, char *argv[])
 {
-    float readings[8] = {0};
+    Serial.println("scan start");
     for (int i=0; i<8; i++) {
         //LED DRIVER: For TLC59208 choose the ledctrl, for PCA9551 choose ledDriver////////////////////
         // ledctrl.on(i);
         ledDriver.setLedState(i, LED_ON);
-        delay(5);
-        adc.waitDRDY(); 
-        readings[i] = adc.readCurrentChannel();
+
+        delay(10);
         //LED DRIVER: For TLC59208 choose the ledctrl, for PCA9551 choose ledDriver////////////////////
         // ledctrl.off(i);
         ledDriver.setLedState(i, LED_OFF);
     }
-
-    for (int i=0; i<8; i++) {
-        Serial.print(readings[i], 5);
-        Serial.print('\t');
-    }
-    Serial.println();
-}
-
-void read_adc(int argc, char *argv[])
-{
-    adc.waitDRDY(); 
-    float val = adc.readCurrentChannel();
-    Serial.println(val , 5);
+    Serial.println("scan done");
+    delay(100);
 }
 
 void led(int argc, char *argv[])
@@ -89,19 +71,15 @@ void help(int argc, char *argv[])
 void setup()
 {
     Serial.begin(9600);
-    SPI.begin();
     Wire.begin();
     // ledctrl.begin();
-    adc.begin(ADS1256_DRATE_30000SPS,ADS1256_GAIN_1,false); 
-    adc.setChannel(0,1);    // differential ADC reading 
 
     cli.add_command({"scan", scan, "Perform a scan sequence: for each led measure adc value"});
-    cli.add_command({"adc", read_adc, "Reads ADC measurement"});
     cli.add_command({"led", led, "Turns an LED <number> on/off <state>.\n\t\t\t\tUsage: led <number> <state>"});
     cli.add_command({"help", help, "Lists all available commands"});
     cli.begin();
 
-    //Serial.println("PlasticScanner is initialized!");
+    Serial.println("PlasticScanner is initialized!");
 }
 
 void loop()
